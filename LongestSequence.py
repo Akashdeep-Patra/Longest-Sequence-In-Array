055df29d572974a9b2a9b7a475eae7a3ba2f9883
Akashdeep Patra <adeep8961@gmail.com>
2020-06-21 13:43:51 +0000

    solution

---
+++ LongestSequence.py
@@ -0,0 +1,29 @@
+def longestSequence(a):
+  mem=dict()
+  visited=set()
+  ans=[]
+  for i,j in enumerate(a):
+    mem[j]=i
+  max_length=0
+  for key in mem.keys():
+    if key in mem:
+      continue
+    value=key
+    length=0
+    collector=[]
+    while(value in mem):
+      collector.append(value)
+      length+=1
+      value+=1
+      visited.add(value)
+    value=key-1
+    while(value in mem):
+      collector.append(value)
+      value-=1
+      length+=1
+      visited.add(value)
+    if(length>max_length):
+      max_length=length
+      ans=collector
+  return ans
+
